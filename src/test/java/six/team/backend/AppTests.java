e2870bece59bb63e3288e6eedc4fc581b47c34aa
kndivanovich <kndivanovich@gmail.com>
2015-09-18 14:18:08 +0100

    Added 1 initial test (gives and exception for now) but will come back when there's more to test on the Register page.

--- src/test/java/six/team/backend/AppTests.java
+++ src/test/java/six/team/backend/AppTests.java
@@ -10,6 +10,11 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -36,4 +41,41 @@ public class AppTests {
                 .andExpect(status().isOk())
                 .andExpect(view().name("hello"));
     }
+
+    private static Connection getDBConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String db = "jdbc:mysql://46.101.32.73:3306/enterprisegym";
+            connection = DriverManager.getConnection(db, "admin", "admin");
+
+
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+
+    @Test
+    public void insertNewUser() {
+
+        Connection connection = null;
+        connection = getDBConnection();
+
+        try {
+            PreparedStatement psUsernameChecker = connection.prepareStatement("INSERT INTO Users (username, password, firstname, lastname, gender, email, contactnumber, country, university, status, subject, year, matricnumber, young_es, usergroup, mobile, activated, token, registration_date) VALUES ('billgates', 'baguettes', 'Bill', 'Gates', 'm', 'bgates@ms.com', '123456', 'UK', 'UoD', 'Undergraduate', 'CompSci', 2015, '123123', 'ES1', 'Students', '077123456', '0', 'token1', '01/01/2015 00:01')");
+
+            // fetch in a result set and count if the results match
+        }
+        catch (SQLException e)
+        {
+            // do something appropriate with the exception, *at least*:
+            e.printStackTrace();
+        }
+
+    }
+
 }
