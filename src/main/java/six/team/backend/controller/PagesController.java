46d4739df6c64460f6b07d54b0b29ce3ea02defa
Gareth Lloyd <g.lloyd@dundee.ac.uk>
2015-09-25 13:01:17 +0100

    fixes to page controller along with permissions

--- src/main/java/six/team/backend/controller/PagesController.java
+++ src/main/java/six/team/backend/controller/PagesController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import six.team.backend.PageJsonGen;
+import six.team.backend.dao.UserDAO;
 import six.team.backend.model.News;
 import six.team.backend.model.Pages;
 import six.team.backend.store.CommentStore;
@@ -26,41 +27,69 @@ public class PagesController {
 
     @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
     public@ResponseBody ResponseEntity<String> addPage(HttpServletRequest request, HttpServletResponse response, @RequestBody String text) {
-        boolean exists= Pages.checkValidity(request.getHeader("title"));
-        JSONObject json = new JSONObject();
-        json.put("exists", exists);
+        UserDAO UD = new UserDAO();
+        String token = request.getHeader("token");
+        if(UD.getUserGroupPermissions(UD.getUserGroup(token),"pageadd")) {
+            boolean exists = Pages.checkValidity(request.getHeader("title"));
+            JSONObject json = new JSONObject();
+            json.put("exists", exists);
 
-        if(exists){
-            return new ResponseEntity<String>(json.toString(), HttpStatus.valueOf(409));
-        }
-        else{
-            boolean success=Pages.addPage(request.getHeader("parentSlug"),request.getHeader("title"),request.getHeader("description"),text,request.getHeader("permission"),Integer.parseInt(request.getHeader("order")));
-            json.put("sucess", success);
-            if(success)
-                return new ResponseEntity<String>(json.toString(), HttpStatus.valueOf(201));
-            else
-                return new ResponseEntity<String>(json.toString(), HttpStatus.valueOf(201));
+            if (exists) {
+                return new ResponseEntity<String>(json.toString(), HttpStatus.valueOf(409));
+            } else {
+                boolean success = Pages.addPage(request.getHeader("parentSlug"), request.getHeader("title"), request.getHeader("description"), text, request.getHeader("permission"), Integer.parseInt(request.getHeader("order")));
+                json.put("sucess", success);
+                if (success)
+                    return new ResponseEntity<String>(json.toString(), HttpStatus.valueOf(201));
+                else
+                    return new ResponseEntity<String>(json.toString(), HttpStatus.valueOf(201));
+            }
+        }else {
+            JSONObject message = new JSONObject();
+            message.put("pages", "You are unauthorized to add a page");
+            return new ResponseEntity<String>(message.toString(), HttpStatus.UNAUTHORIZED);
         }
 
     }
 
     @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE,value = "/{slug}/delete", method = RequestMethod.POST)
     public@ResponseBody ResponseEntity<String> deletePage(HttpServletRequest request, HttpServletResponse response, @PathVariable(value="slug") String slug) {
-        boolean success=Pages.deletePage(slug);
-        if(success)
-            return new ResponseEntity<String>("Page deleted", HttpStatus.valueOf(201));
-        else
-            return new ResponseEntity<String>("Page couldnt be deleted", HttpStatus.valueOf(201));
+        UserDAO UD = new UserDAO();
+        String token = request.getHeader("token");
+        if(UD.getUserGroupPermissions(UD.getUserGroup(token),"pagedelete")) {
+            boolean success = Pages.deletePage(slug);
+            JSONObject json = new JSONObject();
+            json.put("sucess", success);
+            if (success) {
+                return new ResponseEntity<String>(json.toString(), HttpStatus.valueOf(201));
+            } else {
+                return new ResponseEntity<String>(json.toString(), HttpStatus.valueOf(201));
+            }
+        }else {
+            JSONObject message = new JSONObject();
+            message.put("pages", "You are unauthorized to delete a page");
+            return new ResponseEntity<String>(message.toString(), HttpStatus.UNAUTHORIZED);
+        }
     }
 
     @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE,value = "/{slug}/edit", method = RequestMethod.POST)
     public@ResponseBody ResponseEntity<String> editPage(HttpServletRequest request, HttpServletResponse response,@PathVariable(value="slug") String slug, @RequestBody String text) {
-        boolean success=Pages.editPage(slug,request.getHeader("parentSlug"), request.getHeader("title"), request.getHeader("description"), text, request.getHeader("permission"), Integer.parseInt(request.getHeader("order")));
-        if(success)
-            return new ResponseEntity<String>("Page edited", HttpStatus.valueOf(201));
-        else
-            return new ResponseEntity<String>("Page couldn be edited", HttpStatus.valueOf(201));
-
+        UserDAO UD = new UserDAO();
+        String token = request.getHeader("token");
+        if(UD.getUserGroupPermissions(UD.getUserGroup(token),"pagedelete")) {
+            boolean success = Pages.editPage(slug, request.getHeader("parentSlug"), request.getHeader("title"), request.getHeader("description"), text, request.getHeader("permission"), Integer.parseInt(request.getHeader("order")));
+            JSONObject json = new JSONObject();
+            json.put("sucess", success);
+            if (success) {
+                return new ResponseEntity<String>(json.toString(), HttpStatus.valueOf(201));
+            } else {
+                return new ResponseEntity<String>(json.toString(), HttpStatus.valueOf(201));
+            }
+        }else {
+            JSONObject message = new JSONObject();
+            message.put("pages", "You are unauthorized to edit a page");
+            return new ResponseEntity<String>(message.toString(), HttpStatus.UNAUTHORIZED);
+        }
     }
 
     @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
--- src/main/java/six/team/backend/controller/QuizController.java
+++ src/main/java/six/team/backend/controller/QuizController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import six.team.backend.dao.UserDAO;
 import six.team.backend.model.Auth;
 import six.team.backend.model.Quiz;
 import six.team.backend.store.AnswerStore;
@@ -33,69 +34,81 @@ public class QuizController {
     private final static Logger logger = Logger.getLogger(AuthenticationController.class);
 
     @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE,value = "/{quizid}", method = RequestMethod.GET)
-    public @ResponseBody ResponseEntity<String> getAttr(@PathVariable(value="quizid") String id ) {
-        LinkedList<QuestionStore> test = Quiz.getQuiz(id);
-        JSONArray array = new JSONArray();
-        for(int i=0; i<test.size(); i++){
-            JSONObject object = new JSONObject();
-            JSONArray options = new JSONArray();
-           for(int r=-0; r<test.get(i).getAnswers().size(); r++){
-              options.put(test.get(i).getAnswers().get(r).getAnswer_text());
-           }
-            object.put("options", options);
-
-            object.put("answer", test.get(i).getCorrectAnswer());
-            object.put("question", test.get(i).getQuestion_text());
-
-            array.put(object);
+    public @ResponseBody ResponseEntity<String> getQuiz(@PathVariable(value="quizid") String id, HttpServletRequest request, HttpServletResponse res) {
+        UserDAO UD = new UserDAO();
+        String token = request.getHeader("token");
+        if(UD.getUserGroupPermissions(UD.getUserGroup(token),"viewquiz")) {
+            LinkedList<QuestionStore> test = Quiz.getQuiz(id);
+            JSONArray array = new JSONArray();
+            for (int i = 0; i < test.size(); i++) {
+                JSONObject object = new JSONObject();
+                JSONArray options = new JSONArray();
+                for (int r = -0; r < test.get(i).getAnswers().size(); r++) {
+                    options.put(test.get(i).getAnswers().get(r).getAnswer_text());
+                }
+                object.put("options", options);
+                object.put("answer", test.get(i).getCorrectAnswer());
+                object.put("question", test.get(i).getQuestion_text());
+                array.put(object);
+            }
+            return new ResponseEntity<String>(array.toString(), HttpStatus.OK);
+        }else {
+            JSONObject message = new JSONObject();
+            message.put("pages", "You are unauthorized to view this quiz");
+            return new ResponseEntity<String>(message.toString(), HttpStatus.UNAUTHORIZED);
         }
-        return new ResponseEntity<String>(array.toString(), HttpStatus.OK);
     }
 
     @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE,value = "/create", method = RequestMethod.POST)
-    public @ResponseBody ResponseEntity<String> getAttr3(HttpServletRequest request, HttpServletResponse res) {
+    public @ResponseBody ResponseEntity<String> createQuiz(HttpServletRequest request, HttpServletResponse res) {
         request.getParameter("body");
         JSONArray array = null;
         LinkedList<QuestionStore> questions = new LinkedList<QuestionStore>();
         LinkedList<AnswerStore> answers;
-       try {
-           if ("POST".equalsIgnoreCase(request.getMethod()))
-           {
-               String read = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
-               array = new JSONArray(read);
-               for(int i =0; i<array.length(); i++){
-                   String randomQuestionId = UUID.randomUUID().toString();
-                   randomQuestionId = randomQuestionId.replaceAll("-", "");
+        try {
+            if ("POST".equalsIgnoreCase(request.getMethod())) {
+                String read = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+                array = new JSONArray(read);
+                for (int i = 0; i < array.length(); i++) {
+                    String randomQuestionId = UUID.randomUUID().toString();
+                    randomQuestionId = randomQuestionId.replaceAll("-", "");
                     QuestionStore questionStore = new QuestionStore();
                     questionStore.setQuestion_id(randomQuestionId);
-                    JSONObject object =  array.getJSONObject(i);
+                    JSONObject object = array.getJSONObject(i);
                     int answer = object.getInt("answer");
-                    JSONArray options=  object.getJSONArray("options");
-                    String questionText =  object.getString("question");
+                    JSONArray options = object.getJSONArray("options");
+                    String questionText = object.getString("question");
                     questionStore.setQuestion_text(questionText);
                     answers = new LinkedList<AnswerStore>();
-                   for(int r =0; r<options.length(); r++) {
-                       AnswerStore answerStore = new AnswerStore();
-                       String randomAnswerId = UUID.randomUUID().toString();
-                       randomAnswerId = randomAnswerId.replaceAll("-", "");
-                       answerStore.setAnswer_id(randomAnswerId);
-                       answerStore.setAnswer_text(options.get(r).toString());
-                       if(r == answer){
-                           questionStore.setCorrectAnswer(randomAnswerId);
-                       }
-                       answers.add(answerStore);
-                   }
-                   questionStore.setAnswers(answers);
-                   questions.add(questionStore);
-               }
-           }
-       }
-           catch (Exception e){
-           System.out.println(e);
-       }
+                    for (int r = 0; r < options.length(); r++) {
+                        AnswerStore answerStore = new AnswerStore();
+                        String randomAnswerId = UUID.randomUUID().toString();
+                        randomAnswerId = randomAnswerId.replaceAll("-", "");
+                        answerStore.setAnswer_id(randomAnswerId);
+                        answerStore.setAnswer_text(options.get(r).toString());
+                        if (r == answer) {
+                            questionStore.setCorrectAnswer(randomAnswerId);
+                        }
+                        answers.add(answerStore);
+                    }
+                    questionStore.setAnswers(answers);
+                    questions.add(questionStore);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
         String quizTitle = request.getHeader("title");
-        Quiz.createQuiz(questions, quizTitle);
-        return new ResponseEntity<String>(array.toString(), HttpStatus.OK);
+        UserDAO UD = new UserDAO();
+        String token = request.getHeader("token");
+        if(UD.getUserGroupPermissions(UD.getUserGroup(token),"quizcreate")) {
+            Quiz.createQuiz(questions, quizTitle);
+            return new ResponseEntity<String>(array.toString(), HttpStatus.OK);
+        }else {
+            JSONObject message = new JSONObject();
+            message.put("pages", "You are unauthorized to create a quiz");
+            return new ResponseEntity<String>(message.toString(), HttpStatus.UNAUTHORIZED);
+        }
     }
 
     @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
